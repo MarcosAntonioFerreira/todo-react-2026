05386e69c096edc31e43f222c64633cc11d5129d
MarcosAntonioFerreira <marcosantonioalmeida655@gmail.com>
2026-01-31 21:11:01 -0300

    chore: commit inicial de To Do React 2026

---
+++ src/core-components/task-item.tsx
@@ -0,0 +1,120 @@
+import ButtonIcon from "../components/button-icon";
+import Card from "../components/card";
+import InputCheckbox from "../components/input-checkbox";
+import Text from "../components/text";
+import React from "react";
+import TrashIcon from "../assets/icons/trash.svg?react";
+import PencilIcon from "../assets/icons/pencil.svg?react";
+import XIcon from "../assets/icons/x.svg?react";
+import CheckIcon from "../assets/icons/check.svg?react";
+import InputText from "../components/input-text"
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
+import useTask from "../hooks/use-task";
+
+interface TaskItemProps {
+    task: Task;
+}
+
+
+export default function TaskItem({ task }: TaskItemProps) {
+
+    const [isEditing, setIsEditing] = React.useState(task?.state === TaskState.Creating);
+
+    const [taskTitle, setTaskTitle] = React.useState(task.title || "");
+
+    const { updateTask, updateTaskStatus, deleteTask } = useTask();
+
+    function handleEditTask() {
+        setIsEditing(true);
+    }
+
+    function handleExitEditTask() {
+        if (task.state === TaskState.Creating) {
+            deleteTask(task.id);
+        }
+        setIsEditing(false);
+    }
+
+    function handleChangeTaskTitle(e: React.ChangeEvent<HTMLInputElement>) {
+        setTaskTitle(e.target.value || "");
+    }
+
+    function handleSaveTask(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        updateTask(task.id, { title: taskTitle });
+
+        setIsEditing(false);
+    }
+
+    function handleChangeTaskStatus(e: React.ChangeEvent<HTMLInputElement>) {
+        const checked = e.target.checked;
+
+        updateTaskStatus(task.id, checked);
+    }
+
+    function handleDeleteTask() {
+        deleteTask(task.id);
+    }
+
+    return (
+
+        <Card size="md"  >
+            {!isEditing ? (
+                <div className="flex items-center gap-4">
+                    <InputCheckbox
+                        checked={task?.concluded}
+                        onChange={handleChangeTaskStatus}
+                    />
+                    <Text
+                        className={cx("flex-1", {
+                            'line-through': task?.concluded,
+                        })}>{task?.title}</Text>
+                    <div className="flex gap-1">
+                        <ButtonIcon
+                            icon={TrashIcon}
+                            variant="tertiary"
+                            onClick={handleDeleteTask}
+                        />
+                        <ButtonIcon
+                            icon={PencilIcon}
+                            variant="tertiary"
+                            onClick={handleEditTask}
+                        />
+                    </div>
+                </div>
+            )
+                :
+                (
+                    <form onSubmit={handleSaveTask} className="flex items-center gap-4">
+
+                        <InputText
+                            value={taskTitle}
+                            className="flex-1"
+                            onChange={handleChangeTaskTitle}
+                            required
+                            autoFocus
+                        />
+                        <div className="flex gap-1">
+                            <ButtonIcon
+                                type="button"
+                                icon={XIcon}
+                                variant="secondary"
+                                onClick={handleExitEditTask}
+                            />
+                            <ButtonIcon
+                                type="submit"
+                                icon={CheckIcon}
+                                variant="primary"
+
+                            />
+                        </div>
+
+                    </form >
+                )}
+
+        </Card>
+
+    )
+}
